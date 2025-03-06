6d168e03f51ab9406efbdd5d9ab86155c97ecef8
Prencyl Abueg <prencyl_abueg@neun.ph>
2025-03-07 03:14:28 +0800

    Health: Refactored into interface idamageable

--- Assets/Scripts/Gameplay/Stat.cs
+++ Assets/Scripts/Gameplay/Stat.cs
@@ -1,7 +1,5 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Serialization;
-
 [CreateAssetMenu(fileName = "Stat", menuName = "ScriptableObjects/Stat")]
 public class Stat : SerializedScriptableObject
 {
