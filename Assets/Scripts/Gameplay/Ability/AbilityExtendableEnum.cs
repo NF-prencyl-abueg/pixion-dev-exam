52b3317911d957b2e8d7ba5b3491eb8523d45fc5
Prencyl Abueg <prencyl_abueg@neun.ph>
2025-03-06 18:52:25 +0800

    Player Controller: Added player input reader and ability extendable enum

---
+++ Assets/Scripts/Gameplay/Ability/AbilityExtendableEnum.cs
@@ -0,0 +1,18 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Ability", menuName = "ScriptableObjects/Gameplay/AbilityEnum")]
+public class AbilityExtendableEnum : SerializedScriptableObject
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
