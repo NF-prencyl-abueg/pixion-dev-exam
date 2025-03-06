70cc0bf9a44be1f66cc72203682b97911ddfb51b
Prencyl Abueg <prencyl_abueg@neun.ph>
2025-03-07 03:14:16 +0800

    Ability: Cleaned up some code

--- Assets/Scripts/Gameplay/Ability/AbilityList.cs
+++ Assets/Scripts/Gameplay/Ability/AbilityList.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 [CreateAssetMenu(fileName = "New Ability List", menuName = "ScriptableObjects/Ability/Ability List")]
 public class AbilityList : SerializedScriptableObject
