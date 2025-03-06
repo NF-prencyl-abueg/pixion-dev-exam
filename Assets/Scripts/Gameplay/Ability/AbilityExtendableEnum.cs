59c3e0220a7fdd8922f45b1ed0b51ed7728838d5
Prencyl Abueg <prencyl_abueg@neun.ph>
2025-03-06 21:22:02 +0800

    Ability Extendable Enum: Cleaned up the script

--- Assets/Scripts/Gameplay/Ability/AbilityExtendableEnum.cs
+++ Assets/Scripts/Gameplay/Ability/AbilityExtendableEnum.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 [CreateAssetMenu(fileName="Ability", menuName = "ScriptableObjects/Gameplay/AbilityEnum")]
 public class AbilityExtendableEnum : SerializedScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
